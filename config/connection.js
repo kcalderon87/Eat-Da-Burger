a873770c931846202222923e608b05daff8e931e
kcalderon87 <kcalderon87@gmail.com>
2017-01-26 21:45:10 -0500

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,16 @@
 var mysql = require('mysql');
 
+// var connection = mysql.createConnection({
+// 	host: 'mysql4.gear.host',
+// 	user: 'root',
+// 	password: 'process.env.PASS',
+// 	database: 'burgers_db'
+// })
+
 var connection = mysql.createConnection({
-	host: 'mysql4.gear.host',
-	user: 'root',
-	password: 'process.env.PASS',
+	host: 'wp433upk59nnhpoh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+	user: 'q45xf8ldoxnxaohi',
+	password: 'maelcjxw274v6i2x',
 	database: 'burgers_db'
 })
 
