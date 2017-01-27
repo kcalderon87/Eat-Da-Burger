e4bf29809b8d62dd0d330613a865f96349c2e1a2
kcalderon87 <kcalderon87@gmail.com>
2017-01-26 21:26:36 -0500

    updated port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var path = require('path');
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 5000;
 var orm = require('./config/orm.js');
 
 app.use(bodyParser.urlencoded({
