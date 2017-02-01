55271c173db6b86118dd8c985ab94ee174648ec8
kcalderon87 <kcalderon87@gmail.com>
2017-02-01 14:36:17 -0500

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,5 @@
 var mysql = require('mysql');
+var connection;
 
 // var connection = mysql.createConnection({
 // 	host: 'localhost',
@@ -7,12 +8,23 @@ var mysql = require('mysql');
 // 	database: 'burgers_db'
 // })
 
-var connection = mysql.createConnection({
-	host: 'wp433upk59nnhpoh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-	user: 'q45xf8ldoxnxaohi',
-	password: 'maelcjxw274v6i2x',
-	//database: 'burgers_db'
-})
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'burgers_db'
+	})
+}
+	
+
+	// connection = mysql.createConnection({
+	// host: 'wp433upk59nnhpoh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+	// user: 'q45xf8ldoxnxaohi',
+	// password: 'maelcjxw274v6i2x',
+	// //database: 'burgers_db'
 
 connection.connect(function(err) {
 	if (err) {
